8e462317d7cbe0aed977597aef6f21b40631b1ea
Nhlamul096 <107721534+Nhlamul096@users.noreply.github.com>
2022-06-21 08:42:44 +0200

    Update Jobs.java

--- restapp/restapp/src/main/java/org/idealsolution/restapp/joblisting/Jobs.java
+++ restapp/restapp/src/main/java/org/idealsolution/restapp/joblisting/Jobs.java
@@ -4,15 +4,8 @@ package org.idealsolution.restapp.joblisting;
 
 import java.util.List;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
+import javax.ws.rs.*;
+
 
 import org.idealsolution.restapp.hibernate.Entities.ApplicantEntity;
 import org.idealsolution.restapp.hibernate.Entities.JobEntity;
